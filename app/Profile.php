32e9c3c02dcb11796f544e8025166eb5011f8fcc
abhishek-pk <60965491+abhishek-pk@users.noreply.github.com>
2020-08-12 18:45:42 +0530

    first commit

---
+++ app/Profile.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Profile extends Model
+{
+    protected $guarded = [];
+
+    public function profileImage()
+    {
+        $imagePath = ($this->image) ? $this->image : 'profile/WKtTTGUt1gYkziKZ3rjGt36smmhB1tlTXlZ6Jkez.png';
+        return '/storage/'. $imagePath;
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function followers()
+    {
+        return $this->belongsToMany(User::class);
+    }
+}
---
+++ resources/views/emails/welcome-email.blade.php
@@ -0,0 +1,8 @@
+@component('mail::message')
+# Welcome to freecodegram
+
+Welcome to the new freecodegram app.
+
+Thanks,<br>
+Abhishek
+@endcomponent
